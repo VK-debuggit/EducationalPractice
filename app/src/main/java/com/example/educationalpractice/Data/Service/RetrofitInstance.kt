b4697ba231793feac37eec7a72c27769015898b2
Karaseva <vk811966karaseva@yandex.ru>
2025-12-19 12:59:38 +0300

    Создание пользователя в БД

--- app/src/main/java/com/example/educationalpractice/Data/Service/RetrofitInstance.kt
+++ app/src/main/java/com/example/educationalpractice/Data/Service/RetrofitInstance.kt
@@ -27,15 +27,14 @@ import com.example.educationalpractice.data.service.UserManagementService
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.net.InetSocketAddress
+import java.net.Proxy
 
 const val SUPABASE_URL = "https://favuckhcdbijjjmorjbu.supabase.co/"
 
 object RetrofitInstance {
-    //private val proxy = Proxy(Proxy.Type.HTTP, InetSocketAddress("10.207.106.71", 3128))
-    //private val client = OkHttpClient.Builder().proxy(proxy).build()
-
-    private val client = OkHttpClient.Builder()
-        .build()
+    private val proxy = Proxy(Proxy.Type.HTTP, InetSocketAddress("10.207.106.71", 3128))
+    private val client = OkHttpClient.Builder().proxy(proxy).build()
 
     private val retrofit = Retrofit.Builder()
         .baseUrl(SUPABASE_URL)
